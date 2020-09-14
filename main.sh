8c0d2fb0f65a885933a3f46e2b82f48b9c7e832c
Lav Chaudhary <lav.chaudhary5@gmail.com>
2020-09-14 15:42:45 +0530

    third commit

--- main.sh
+++ main.sh
@@ -1,4 +1,1 @@
-cd C:\\Users\\lavch\\Downloads
-ssh -i "test_key_value_pair_1.pem" ubuntu@ec2-3-134-110-218.us-east-2.compute.amazonaws.com
-cd /home/ubuntu/
-touch temp_file.txt
+time > execute_time.txt
